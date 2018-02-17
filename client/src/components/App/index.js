5ca3f5379177eb33cd3565234aeab47d206d084f
Merek Orlowski <merekorlowski@gmail.com>
2018-02-17 15:19:50 -0500

    Initial commit

---
+++ client/src/components/App/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          
+        </header>
+        <section>
+
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
